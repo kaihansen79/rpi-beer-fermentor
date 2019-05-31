e4dae6d4f2824e4a9f79a2dbf28cd9f21624ff6b
Kai Hansen <kaihansen79@gmail.com>
2019-05-31 11:14:40 -0700

    Update fermentor.py

--- fermentor.py
+++ fermentor.py
@@ -21,6 +21,19 @@ headers = { 'Content-Type': 'application/json' }
 relayState = 0
 lastTemp = 0
 
+def sendToEs(ct, rs):
+    log_uuid = str(uuid.uuid1())
+    date_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    data = { 'currentTemp': ct, 'relayState': rs, 'date_time': date_time }
+    json_data = json.dumps(data)
+
+    try:
+        r = requests.put('http://' + esIp + ':9200/fermentor-' + str(datetime.now().year) + '/_doc/' + log_uuid, data=json_data, headers=headers)
+    except Exception as e:
+        print('error in try: ' + str(e))
+        
+    print('currentTemp: ' + str(ct) + ' | relayState: ' + str(rs))
+
 while True:
     tempfile = open("/sys/bus/w1/devices/28-051680360fff/w1_slave")
     thetext = tempfile.read()
@@ -34,22 +47,13 @@ while True:
         print ('relay off')
         GPIO.output(18, GPIO.LOW)
         relayState = 0
+        sendToEs(temperature, relayState)
             
     elif temperature < desiredTemperature-0.05 and relayState == 0:
         # turn on relay
         print ('relay on')
         GPIO.output(18, GPIO.HIGH)
         relayState = 1
-
-    log_uuid = str(uuid.uuid1())
-    date_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-    data = { 'currentTemp': temperature, 'relayState': relayState, 'date_time': date_time }
-    json_data = json.dumps(data)
-
-    try:
-        r = requests.put('http://' + esIp + ':9200/fermentor-' + str(datetime.now().year) + '/_doc/' + log_uuid, data=json_data, headers=headers)
-        print('currentTemp: ' + str(temperature))
-    except Exception as e:
-        print('error in try: ' + str(e))
+        sendToEs(temperature, relayState)
 
     time.sleep(30)
